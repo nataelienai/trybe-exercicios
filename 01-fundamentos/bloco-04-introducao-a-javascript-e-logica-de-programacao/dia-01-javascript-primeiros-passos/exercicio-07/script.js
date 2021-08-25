a3480f871dc4c9921ec8b4af0900669423ef8221
Natã Elienai <nata.elienai@gmail.com>
2021-08-25 19:40:12 -0300

    Exercicio 7: programa que converte uma nota dada em porcentagem em conceitos de A a F

---
+++ 01-fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-01-javascript-primeiros-passos/exercicio-07/script.js
@@ -0,0 +1,19 @@
+let grade = 55;
+
+if (grade > 100 || grade < 0) {
+  console.log("ERRO: Porcentagem inválida!");
+}
+
+if (grade >= 90) {
+  console.log("A");
+} else if (grade >= 80) {
+  console.log("B");
+} else if (grade >= 70) {
+  console.log("C");
+} else if (grade >= 60) {
+  console.log("D");
+} else if (grade >= 50) {
+  console.log("E");
+} else {
+  console.log("F");
+}
