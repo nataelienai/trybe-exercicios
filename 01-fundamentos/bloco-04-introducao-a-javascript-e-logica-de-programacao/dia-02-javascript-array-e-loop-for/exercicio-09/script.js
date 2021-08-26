9e6cc6257d381404bfb800b0b04de5793aa92cf8
Natã Elienai <nata.elienai@gmail.com>
2021-08-26 16:44:34 -0300

    Exercicio 9: calculando a divisao dos numeros de um array criado por um loop for

---
+++ 01-fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-02-javascript-array-e-loop-for/exercicio-09/script.js
@@ -0,0 +1,11 @@
+let numbers = [];
+
+for (number = 1; number <= 25; number += 1) {
+  numbers.push(number);
+}
+
+console.log("array: " + numbers);
+
+for (number of numbers) {
+  console.log(number + "/" + 2 + " = " + number / 2);
+}
