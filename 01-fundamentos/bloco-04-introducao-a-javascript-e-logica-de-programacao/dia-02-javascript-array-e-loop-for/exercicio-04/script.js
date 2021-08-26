f586332389bb093c39d7ec232a3b0b9dba489f51
Natã Elienai <nata.elienai@gmail.com>
2021-08-26 16:14:38 -0300

    Exercicio 4: calculando a media aritmetica dos numeros de um array com funcionalidade extra

---
+++ 01-fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-02-javascript-array-e-loop-for/exercicio-04/script.js
@@ -0,0 +1,15 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let total = 0;
+
+for (number of numbers) {
+  total += number;
+}
+
+let average = total / numbers.length;
+console.log(average);
+
+if (average > 20) {
+  console.log("valor maior que 20");
+} else {
+  console.log("valor menor ou igual a 20");
+}
