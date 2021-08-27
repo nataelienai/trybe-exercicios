9311c8e63d2db3f06962a002f2809ae146b3914f
Natã Elienai <nata.elienai@gmail.com>
2021-08-27 17:33:43 -0300

    Exercicio 2: programa que imprime um triangulo retangulo de asteriscos

---
+++ 01-fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-03-javascript-logica-de-programacao-e-algoritmos/exercicio-02/script.js
@@ -0,0 +1,9 @@
+let n = 5;
+
+for (let ln = 0; ln < n; ln += 1) {
+  let asteriskLine = "";
+  for (let col = 0; col <= ln; col += 1) {
+    asteriskLine += "*";
+  }
+  console.log(asteriskLine);
+}
