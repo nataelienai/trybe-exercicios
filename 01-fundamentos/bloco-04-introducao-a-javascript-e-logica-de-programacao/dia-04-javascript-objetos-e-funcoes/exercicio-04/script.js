6dab5aad9aa151dd6b5db1be28ff856b9aecf44e
Natã Elienai <nata.elienai@gmail.com>
2021-08-30 17:11:27 -0300

    Correcao: criando e separando os arquivos dos exercicios 4 e 5

--- 01-fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-04-javascript-objetos-e-funcoes/exercicio-04/script.js
+++ 01-fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-04-javascript-objetos-e-funcoes/exercicio-04/script.js
@@ -5,21 +5,6 @@ let info = {
   recorrente: 'Sim'
 };
 
-let info2 = {
-  personagem: 'Tio Patinhas',
-  origem: "Christmas on Bear Mountain, Dell's Four Color Comics #178",
-  nota: 'O último MacPatinhas',
-  recorrente: 'Sim'
-};
-
 for (let key in info) {
-  if (key != 'recorrente') {
-    console.log(info[key] + ' e ' + info2[key]);
-  } else {
-    if (info[key] == 'Sim' && info2[key] == 'Sim') {
-      console.log('Ambos recorrentes');
-    } else {
-      console.log(info[key] + ' e ' + info2[key]);
-    }
-  }
+  console.log(info[key]);
 }
