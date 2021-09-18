8812a169047ab59485d183953b1926ca496a8fb0
Natã Elienai <nata.elienai@gmail.com>
2021-09-18 16:29:55 -0300

    added a job start date input field and its validation script

--- 01-fundamentos/bloco-06-html-e-css-forms-flexbox-e-responsivo/dia-01-html-css-forms/script.js
+++ 01-fundamentos/bloco-06-html-e-css-forms-flexbox-e-responsivo/dia-01-html-css-forms/script.js
@@ -44,3 +44,50 @@ function generateStateOptions() {
 }
 
 window.onload = generateStateOptions;
+
+function getDateFromString(date) {
+  const units = date.split('/');
+  if (units.length !== 3) {
+    return null;
+  }
+
+  if (units[0].length === 0 || units[1].length === 0
+    || units[2].length === 0) {
+    return null;
+  }
+
+  return {
+    day: Number(units[0]),
+    month: Number(units[1]),
+    year: Number(units[2]),
+  };
+}
+
+function isStartDateValid(event) {
+  const startDateInput = document.getElementById('start-date-input');
+  const date = getDateFromString(startDateInput.value);
+
+  if (date === null) {
+    window.alert('ERRO: O formato da data de início é inválido!');
+    event.preventDefault();
+    return;
+  }
+  if (date.day < 1 || date.day > 31) {
+    window.alert('ERRO: O dia da data de início é inválido!');
+    event.preventDefault();
+    return;
+  }
+  if (date.month < 1 || date.month > 12) {
+    window.alert('ERRO: O mês da data de início é inválido!');
+    event.preventDefault();
+    return;
+  }
+  if (date.year < 0) {
+    window.alert('ERRO: O ano não pode ser negativo!');
+    event.preventDefault();
+    return;
+  }
+}
+
+const form = document.querySelector('form');
+form.addEventListener('submit', isStartDateValid);
