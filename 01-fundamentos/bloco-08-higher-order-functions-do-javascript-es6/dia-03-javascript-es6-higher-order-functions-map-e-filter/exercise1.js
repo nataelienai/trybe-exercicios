d449709b8056079e28b936557ecc1749f7e5b684
Natã Elienai <nata.elienai@gmail.com>
2021-10-09 09:14:20 -0300

    Complete exercise 1 using the Map higher order function

--- 01-fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-03-javascript-es6-higher-order-functions-map-e-filter/exercise1.js
+++ 01-fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-03-javascript-es6-higher-order-functions-map-e-filter/exercise1.js
@@ -1,3 +1,5 @@
+const assert = require('assert');
+
 const books = [
   {
     id: 1,
@@ -61,4 +63,19 @@ const books = [
   },
 ];
 
-// Adicione o código do exercício aqui:
+const expectedResult = [
+  'As Crônicas de Gelo e Fogo - Fantasia - George R. R. Martin',
+  'O Senhor dos Anéis - Fantasia - J. R. R. Tolkien',
+  'Fundação - Ficção Científica - Isaac Asimov',
+  'Duna - Ficção Científica - Frank Herbert',
+  'A Coisa - Terror - Stephen King',
+  'O Chamado de Cthulhu - Terror - H. P. Lovecraft',
+];
+
+function formattedBookNames() {
+  return books.map((book) => (
+    `${book.name} - ${book.genre} - ${book.author.name}`
+  ));
+}
+
+assert.deepStrictEqual(formattedBookNames(), expectedResult);
