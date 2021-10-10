680cb334e189f5c6e37408b2551c076f5a5074fa
Natã Elienai <nata.elienai@gmail.com>
2021-10-10 11:50:21 -0300

    Complete exercise 5 using array destructuring

--- 01-fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-05-javascript-es6-spread-operator-parametro-rest-destructuring-e-mais/exercise5.js
+++ 01-fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-05-javascript-es6-spread-operator-parametro-rest-destructuring-e-mais/exercise5.js
@@ -1,3 +1,10 @@
+const assert = require('assert');
+
 const myList = [1, 2, 3];
 
-// escreva swap abaixo
+const swap = ([elem1, elem2, elem3]) => [elem3, elem2, elem1];
+
+const expected = [3, 2, 1];
+const received = swap(myList);
+
+assert.deepStrictEqual(received, expected);
