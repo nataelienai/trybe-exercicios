5f52ba999f06f055a7b036740212bd0647a1ad16
Natã Elienai <nata.elienai@gmail.com>
2021-10-09 21:46:12 -0300

    Fix and complete exercise 5

--- 01-fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-04-javascript-es6-higher-order-functions-reduce/exercise5.js
+++ 01-fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-04-javascript-es6-higher-order-functions-reduce/exercise5.js
@@ -6,8 +6,15 @@ const names = [
   'Abeladerco', 'Adieidy', 'Alarucha',
 ];
 
+const expectedResult = 20;
+
 function containsA() {
-  // escreva seu código aqui
+  return names.reduce((acc, name) => {
+    const count = name
+      .split('')
+      .reduce((acc, char) => (char === 'a' || char === 'A' ? acc + 1 : acc), 0);
+    return acc + count;
+  }, 0);
 }
 
 assert.deepStrictEqual(containsA(), expectedResult);
