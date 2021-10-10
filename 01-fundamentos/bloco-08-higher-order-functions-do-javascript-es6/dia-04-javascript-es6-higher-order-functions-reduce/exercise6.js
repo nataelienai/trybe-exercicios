1c4aa1c0ee20ce5777128b1385e65140c0862a78
Natã Elienai <nata.elienai@gmail.com>
2021-10-09 21:47:58 -0300

    Add exercise 6 template

---
+++ 01-fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-04-javascript-es6-higher-order-functions-reduce/exercise6.js
@@ -0,0 +1,16 @@
+const assert = require('assert');
+
+const students = ['Pedro Henrique', 'Miguel', 'Maria Clara'];
+const grades = [[9, 8, 10, 7, 5], [10, 9, 9, 10, 8], [10, 7, 10, 8, 9]];
+
+const expectedResult = [
+  { name: 'Pedro Henrique', average: 7.8 },
+  { name: 'Miguel', average: 9.2 },
+  { name: 'Maria Clara', average: 8.8 },
+];
+
+function studentAverage() {
+  // escreva seu código aqui
+}
+
+assert.deepStrictEqual(studentAverage(), expectedResult);
