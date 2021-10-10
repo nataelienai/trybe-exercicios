e603940cde6c5508906d8b3158e2d8b02c7bd434
Natã Elienai <nata.elienai@gmail.com>
2021-10-10 12:09:42 -0300

    Complete exercise 6 using array destructuring

--- 01-fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-05-javascript-es6-spread-operator-parametro-rest-destructuring-e-mais/exercise6.js
+++ 01-fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-05-javascript-es6-spread-operator-parametro-rest-destructuring-e-mais/exercise6.js
@@ -1,5 +1,15 @@
+const assert = require('assert');
+
 const palio = ['Palio', 'Fiat', 2019];
 const shelbyCobra = ['Shelby Cobra', 'Ford', 1963];
 const chiron = ['Chiron', 'Bugatti', 2016];
 
-// escreva toObject abaixo
+const toObject = ([ model, brand, year ]) => ({ model, brand, year });
+
+const palioObject = {model: 'Palio', brand: 'Fiat', year: 2019};
+const shelbyCobraObject = {model: 'Shelby Cobra', brand: 'Ford', year: 1963};
+const chironObject = {model: 'Chiron', brand: 'Bugatti', year: 2016};
+
+assert.deepStrictEqual(toObject(palio), palioObject);
+assert.deepStrictEqual(toObject(shelbyCobra), shelbyCobraObject);
+assert.deepStrictEqual(toObject(chiron), chironObject);
