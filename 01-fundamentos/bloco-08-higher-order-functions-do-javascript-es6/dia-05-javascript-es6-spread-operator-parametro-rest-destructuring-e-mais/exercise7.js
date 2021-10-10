475fbb10936d2641fcdadf2638835f8ed43efae8
Natã Elienai <nata.elienai@gmail.com>
2021-10-10 12:16:20 -0300

    Complete exercise 7 using object destructuring

--- 01-fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-05-javascript-es6-spread-operator-parametro-rest-destructuring-e-mais/exercise7.js
+++ 01-fundamentos/bloco-08-higher-order-functions-do-javascript-es6/dia-05-javascript-es6-spread-operator-parametro-rest-destructuring-e-mais/exercise7.js
@@ -16,8 +16,11 @@ const ships = [
   },
 ];
 
-// escreva shipLength abaixo
+const shipLength = ({ name, length, measurementUnit }) => (
+  `${name} is ${length} ${measurementUnit} long`
+);
 
-console.log(shipLength(ships[0])); // 'Titanic is 269.1 meters long'
-console.log(shipLength(ships[1])); // 'Queen Mary 2 is 1132 feet long'
-console.log(shipLength(ships[2])); // 'Yamato is 256 meters long'
+ships.forEach((ship) => console.log(shipLength(ship)));
+// 'Titanic is 269.1 meters long'
+// 'Queen Mary 2 is 1132 feet long'
+// 'Yamato is 256 meters long'
