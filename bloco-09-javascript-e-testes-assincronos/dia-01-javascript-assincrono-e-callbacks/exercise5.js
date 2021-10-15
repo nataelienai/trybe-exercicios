d627ae4509820c703e89eb3a96f5d74fed404585
Natã Elienai <nata.elienai@gmail.com>
2021-10-15 18:22:02 -0300

    Complete exercise 5

--- bloco-09-javascript-e-testes-assincronos/dia-01-javascript-assincrono-e-callbacks/exercise5.js
+++ bloco-09-javascript-e-testes-assincronos/dia-01-javascript-assincrono-e-callbacks/exercise5.js
@@ -14,6 +14,10 @@ const greet = (temperature) =>
   console.log(`Hi there! Curiosity here. Right now is ${temperature}ºC at Mars`);
 
 // definição da função sendMarsTemperature...
+const sendMarsTemperature = (callback) => {
+  const marsTemperature = getMarsTemperature();
+  setTimeout(() => callback(marsTemperature), messageDelay());
+};
 
 sendMarsTemperature(temperatureInFahrenheit); // imprime "It is currently 47ºF at Mars", por exemplo
 sendMarsTemperature(greet); // imprime "Hi there! Curiosity here. Right now is 53ºC at Mars", por exemplo
