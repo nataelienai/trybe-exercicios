b579cc03047100c5db94d33f7c45d681f2cc8b82
Natã Elienai <nata.elienai@gmail.com>
2021-10-19 19:52:04 -0300

    Complete exercise 1

--- 01-fundamentos/bloco-09-javascript-e-testes-assincronos/dia-03-jest-testes-assincronos/exercise1.test.js
+++ 01-fundamentos/bloco-09-javascript-e-testes-assincronos/dia-03-jest-testes-assincronos/exercise1.test.js
@@ -1,6 +1,5 @@
-// githubApi.test.js
-
 const fetch = require('node-fetch');
+const url = 'https://api.github.com/orgs/tryber/repos';
 
 const getRepos = (url) => (
   fetch(url)
@@ -8,4 +7,11 @@ const getRepos = (url) => (
     .then((data) => data.map((repo) => repo.name))
 );
 
-// implemente seus testes aqui
+describe('the getRepos function', () => {
+  it('returns the repositories from the tryber organization', async () => {
+    const repositories = await getRepos(url);
+
+    expect(repositories).toContain('sd-01-week4-5-project-todo-list');
+    expect(repositories).toContain('sd-01-week4-5-project-meme-generator');
+  });
+});
