ac1c89b2565cfc9b0322cceafe5dc05879907d9c
Natã Elienai <nata.elienai@gmail.com>
2022-02-23 15:50:37 -0300

    feat(bonus): complete exercise 8

--- back-end/bloco-20-introducao-a-sql/dia-3-filtrando-dados-de-forma-especifica/bonus/exercises.sql
+++ back-end/bloco-20-introducao-a-sql/dia-3-filtrando-dados-de-forma-especifica/bonus/exercises.sql
@@ -8,19 +8,19 @@ SELECT * FROM Scientists WHERE Name LIKE '%E%';
 
 /*
 2. Escreva uma query para exibir o nome de todos os projetos cujo o código inicie
-com a letra A . Ordene o resultado em ordem alfabética. 
+com a letra 'A'. Ordene o resultado em ordem alfabética. 
 */
 SELECT Name FROM Projects WHERE Code LIKE 'A%';
 
 /*
 3. Escreva uma query para exibir o código e nome de todos os projetos que possuam
-em seu código o número 3 . Ordene o resultado em ordem alfabética. 
+em seu código o número 3. Ordene o resultado em ordem alfabética. 
 */
 SELECT Code, Name FROM Projects WHERE Code LIKE '%3%' ORDER BY Name;
 
 /*
 4. Escreva uma query para exibir todos os cientistas (valores numéricos) cujos
-projetos sejam AeH3 , Ast3 ou Che1. 
+projetos sejam AeH3, Ast3 ou Che1. 
 */
 SELECT Scientist FROM AssignedTo WHERE Project IN ('AeH3', 'Ast3', 'Che1');
 
@@ -41,3 +41,9 @@ SELECT * FROM Projects WHERE Hours BETWEEN 250 AND 800;
 nome NÃO inicie com a letra 'A'. 
 */
 SELECT Name, Code FROM Projects WHERE Name NOT LIKE 'A%';
+
+/*
+8. Escreva uma query para exibir o nome de todos os projetos cujo código
+contenha a letra 'H'.
+*/
+SELECT Name FROM Projects WHERE Code LIKE '%H%';
