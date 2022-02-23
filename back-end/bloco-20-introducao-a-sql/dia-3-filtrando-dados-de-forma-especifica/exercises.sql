08bd8ade291100e54a1a10c4fe2a0d22895add00
Natã Elienai <nata.elienai@gmail.com>
2022-02-23 15:06:21 -0300

    feat: complete exercise 2

--- back-end/bloco-20-introducao-a-sql/dia-3-filtrando-dados-de-forma-especifica/exercises.sql
+++ back-end/bloco-20-introducao-a-sql/dia-3-filtrando-dados-de-forma-especifica/exercises.sql
@@ -2,3 +2,9 @@ USE PecasFornecedores;
 
 -- 1. Escreva uma query para exibir todas as peças que começam com "GR".
 SELECT * FROM Pecas WHERE name LIKE 'GR%';
+
+/*
+2. Escreva uma query para exibir todos os fornecimentos que contenham a peça com code 2. 
+Organize o resultado por ordem alfabética de fornecedor.
+*/
+SELECT * FROM Fornecimentos WHERE peca = 2;
