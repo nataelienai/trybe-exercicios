65e57177186397d7bf359af4bef331afea998062
Natã Elienai <nata.elienai@gmail.com>
2022-02-23 15:46:22 -0300

    feat(bonus): complete exercise 6

--- back-end/bloco-20-introducao-a-sql/dia-3-filtrando-dados-de-forma-especifica/bonus/exercises.sql
+++ back-end/bloco-20-introducao-a-sql/dia-3-filtrando-dados-de-forma-especifica/bonus/exercises.sql
@@ -29,3 +29,9 @@ SELECT Scientist FROM AssignedTo WHERE Project IN ('AeH3', 'Ast3', 'Che1');
 mais de 500 horas. 
 */
 SELECT * FROM Projects WHERE Hours > 500;
+
+/*
+6. Escreva uma query para exibir todas as informações de todos os projetos
+cujas horas sejam maiores que 250 e menores 800. 
+*/
+SELECT * FROM Projects WHERE Hours BETWEEN 250 AND 800;
