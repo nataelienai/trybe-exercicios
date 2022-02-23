25215f810e41d09dfb0bc4955e64a3d9ef902ff5
Natã Elienai <nata.elienai@gmail.com>
2022-02-23 15:38:29 -0300

    feat(bonus): complete exercise 2

--- back-end/bloco-20-introducao-a-sql/dia-3-filtrando-dados-de-forma-especifica/bonus/exercises.sql
+++ back-end/bloco-20-introducao-a-sql/dia-3-filtrando-dados-de-forma-especifica/bonus/exercises.sql
@@ -5,3 +5,9 @@ USE Scientists;
 possuam a letra 'e' em seu nome.
 */
 SELECT * FROM Scientists WHERE Name LIKE '%E%';
+
+/*
+2.  Escreva uma query para exibir o nome de todos os projetos cujo o código inicie
+com a letra A . Ordene o resultado em ordem alfabética. 
+*/
+SELECT Name FROM Projects WHERE Code LIKE 'A%';
