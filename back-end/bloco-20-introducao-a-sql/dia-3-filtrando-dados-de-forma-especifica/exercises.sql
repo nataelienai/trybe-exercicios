59845ff7a3c513e32516b6bf295de4966ae0a3a2
Natã Elienai <nata.elienai@gmail.com>
2022-02-23 15:11:28 -0300

    feat: complete exercise 4

--- back-end/bloco-20-introducao-a-sql/dia-3-filtrando-dados-de-forma-especifica/exercises.sql
+++ back-end/bloco-20-introducao-a-sql/dia-3-filtrando-dados-de-forma-especifica/exercises.sql
@@ -14,3 +14,9 @@ SELECT * FROM Fornecimentos WHERE peca = 2;
 em que o código do fornecedor tenha a letra N . 
 */
 SELECT peca, Preco, Fornecedor FROM Fornecimentos WHERE Fornecedor LIKE '%N%';
+
+/*
+4. Escreva uma query para exibir todas as informações dos fornecedores que são empresas limitadas (LTDA).
+Ordene os resultados em ordem alfabética decrescente. 
+*/
+SELECT * FROM Fornecedores WHERE name LIKE '%LTDA%' ORDER BY name DESC;
