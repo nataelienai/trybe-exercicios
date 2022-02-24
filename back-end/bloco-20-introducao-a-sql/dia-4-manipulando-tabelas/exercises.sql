ed7a0058d5e1cd68438c5a19a0b9c137905b0d54
Natã Elienai <nata.elienai@gmail.com>
2022-02-24 13:02:59 -0300

    feat(20.4): complete exercise 10

--- back-end/bloco-20-introducao-a-sql/dia-4-manipulando-tabelas/exercises.sql
+++ back-end/bloco-20-introducao-a-sql/dia-4-manipulando-tabelas/exercises.sql
@@ -97,3 +97,14 @@ no mercado interno.
 UPDATE BoxOffice
 SET rating = 6
 WHERE domestic_sales > 200000000 AND international_sales < 300000000;
+
+/*
+10. Exclua da tabela Movies todos os filmes com menos de 100 minutos de duração.
+*/
+DELETE FROM BoxOffice
+WHERE movie_id IN (
+	SELECT id FROM Movies WHERE length_minutes < 100
+);
+
+DELETE FROM Movies
+WHERE length_minutes < 100;
