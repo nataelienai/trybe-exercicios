d1276e5a745d373fc46046d857d4ca956a1af3a5
Natã Elienai <nata.elienai@gmail.com>
2022-02-24 12:59:02 -0300

    feat(20.4): complete exercise 9

--- back-end/bloco-20-introducao-a-sql/dia-4-manipulando-tabelas/exercises.sql
+++ back-end/bloco-20-introducao-a-sql/dia-4-manipulando-tabelas/exercises.sql
@@ -88,3 +88,12 @@ que lucraram mais de 400 milhões no mercado interno.
 UPDATE BoxOffice
 SET rating = 9
 WHERE domestic_sales > 400000000;
+
+/*
+9. Altere a classificação da tabela BoxOffice para 6.0 de todos os filmes que
+lucraram menos de 300 milhões no mercado internacional e mais de 200 milhões
+no mercado interno.
+*/
+UPDATE BoxOffice
+SET rating = 6
+WHERE domestic_sales > 200000000 AND international_sales < 300000000;
