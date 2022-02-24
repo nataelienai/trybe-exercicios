50bec2daa14353263cd14aa760447976ece25d5d
Natã Elienai <nata.elienai@gmail.com>
2022-02-24 11:50:18 -0300

    feat(20.4): complete exercise 2

--- back-end/bloco-20-introducao-a-sql/dia-4-manipulando-tabelas/exercises.sql
+++ back-end/bloco-20-introducao-a-sql/dia-4-manipulando-tabelas/exercises.sql
@@ -14,3 +14,12 @@ VALUES
 	('Procurando Nemo', 'John Lasseter', 2003, 107),
 	('Os Incríveis', 'Brad Bird', 2004, 116),
 	('WALL-E', 'Pete Docter', 2008, 104);
+
+/*
+2. Procurando Nemo foi aclamado pela crítica! Foi classificado em 6.8,
+fez 450 milhões no mercado interno e 370 milhões no mercado internacional.
+Adicione as informações à tabela BoxOffice. 
+*/
+INSERT INTO BoxOffice (movie_id, rating, domestic_sales, international_sales)
+VALUES
+	(9, 6.8, 450000000, 370000000);
