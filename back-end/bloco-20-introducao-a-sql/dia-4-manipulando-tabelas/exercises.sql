f76752b45c8e4a5950935c0c25df3ce7dfe53657
Natã Elienai <nata.elienai@gmail.com>
2022-02-24 12:50:23 -0300

    feat(20.4): complete exercise 7

--- back-end/bloco-20-introducao-a-sql/dia-4-manipulando-tabelas/exercises.sql
+++ back-end/bloco-20-introducao-a-sql/dia-4-manipulando-tabelas/exercises.sql
@@ -69,3 +69,14 @@ WHERE movie_id IN (
 
 DELETE FROM Movies
 WHERE title = 'WALL-E';
+
+/*
+7. Exclua da tabela Movies todos os filmes dirigidos por "Andrew Staton".
+*/
+DELETE FROM BoxOffice
+WHERE movie_id IN (
+	SELECT id FROM Movies WHERE director = 'Andrew Staton'
+);
+
+DELETE FROM Movies
+WHERE director = 'Andrew Staton';
