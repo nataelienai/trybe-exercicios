83360b947dab810c1d6c032591c0f5e2ff68c8ee
Natã Elienai <nata.elienai@gmail.com>
2022-02-28 19:41:27 -0300

    feat(21.1): complete exercise 15

--- back-end/bloco-21-funcoes-sql-joins-e-normalizacao/dia-1-funcoes-mais-usadas-no-sql/exercises.sql
+++ back-end/bloco-21-funcoes-sql-joins-e-normalizacao/dia-1-funcoes-mais-usadas-no-sql/exercises.sql
@@ -101,3 +101,10 @@ funcionário: id , primeiro nome e dia do mês no qual foi contratado (exiba
 somente o dia).
 */
 SELECT employee_id, first_name, DAY(hire_date) FROM employees;
+
+/*
+15. Escreva uma query que exiba as seguintes informações de cada
+funcionário: id , primeiro nome e mês no qual foi contratado (exiba
+somente o mês).
+*/
+SELECT employee_id, first_name, MONTH(hire_date) FROM employees;
