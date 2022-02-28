bf9534dbd42f1b2729688e654251272d22b79f04
Natã Elienai <nata.elienai@gmail.com>
2022-02-28 19:07:56 -0300

    feat(21.1): complete exercise 4

--- back-end/bloco-21-funcoes-sql-joins-e-normalizacao/dia-1-funcoes-mais-usadas-no-sql/exercises.sql
+++ back-end/bloco-21-funcoes-sql-joins-e-normalizacao/dia-1-funcoes-mais-usadas-no-sql/exercises.sql
@@ -15,3 +15,9 @@ SELECT MAX(salary) - MIN(salary) FROM employees;
 pela média salarial em ordem decrescente.
 */
 SELECT job_id, AVG(salary) FROM employees GROUP BY job_id;
+
+/*
+4. Escreva uma query que exiba a quantidade de dinheiro necessária para
+realizar o pagamento de todas as pessoas funcionárias.
+*/
+SELECT SUM(salary) FROM employees;
