ddc047d9c989e0ffb8cd43018091dfc07a6bbf5f
Natã Elienai <nata.elienai@gmail.com>
2022-03-03 13:12:28 -0300

    feat(21.2): complete exercise 5

--- back-end/bloco-21-funcoes-sql-joins-e-normalizacao/dia-2-descomplicando-joins/exercises.sql
+++ back-end/bloco-21-funcoes-sql-joins-e-normalizacao/dia-2-descomplicando-joins/exercises.sql
@@ -27,12 +27,23 @@ FROM Movies
 INNER JOIN BoxOffice ON BoxOffice.movie_id = Movies.id;
 
 /*
-Exercício 4: Utilizando o LEFT JOIN, faça uma busca que retorne todos os
-dados dos cinemas, mesmo os que não possuem filmes em cartaz e,
-adicionalmente, os dados dos filmes que estão em cartaz nestes cinemas.
-Retorne os nomes dos cinemas em ordem alfabética.
+4. Utilizando o LEFT JOIN, faça uma busca que retorne todos os dados dos
+cinemas, mesmo os que não possuem filmes em cartaz e, adicionalmente, os dados
+dos filmes que estão em cartaz nestes cinemas. Retorne os nomes dos cinemas em
+ordem alfabética.
 */
 SELECT *
 FROM Theater
 LEFT JOIN Movies ON Movies.theater_id = Theater.id
+ORDER BY Theater.name;
+
+/*
+5. Utilizando o RIGHT JOIN, faça uma busca que retorne todos os dados dos
+filmes, mesmo os que não estão em cartaz e, adicionalmente, os dados dos
+cinemas que possuem estes filmes em cartaz. Retorne os nomes dos cinemas em
+ordem alfabética.
+*/
+SELECT *
+FROM Theater
+RIGHT JOIN Movies ON Theater.id = Movies.theater_id
 ORDER BY Theater.name;
