22236257d9f867b26c9b5ab202dc3e70e2854bf6
Natã Elienai <nata.elienai@gmail.com>
2022-03-03 13:02:21 -0300

    feat(21.2): complete exercise 3

--- back-end/bloco-21-funcoes-sql-joins-e-normalizacao/dia-2-descomplicando-joins/exercises.sql
+++ back-end/bloco-21-funcoes-sql-joins-e-normalizacao/dia-2-descomplicando-joins/exercises.sql
@@ -1,7 +1,7 @@
 USE Pixar;
 
 /*
-1. Utilizando o INNER JOIN , encontre as vendas nacionais (domestic_sales) e
+1. Utilizando o INNER JOIN, encontre as vendas nacionais (domestic_sales) e
 internacionais (international_sales) de cada filme.
 */
 SELECT id, title, domestic_sales, international_sales
@@ -9,7 +9,7 @@ FROM Movies
 INNER JOIN BoxOffice ON BoxOffice.movie_id = Movies.id;
 
 /*
-2. Utilizando o INNER JOIN , faça uma busca que retorne o número de vendas para
+2. Utilizando o INNER JOIN, faça uma busca que retorne o número de vendas para
 cada filme que possui um número maior de vendas internacionais
 (international_sales) do que vendas nacionais (domestic_sales). 
 */
@@ -17,3 +17,11 @@ SELECT id, title, domestic_sales, international_sales
 FROM Movies
 INNER JOIN BoxOffice ON BoxOffice.movie_id = Movies.id
 WHERE international_sales > domestic_sales;
+
+/*
+3. Utilizando o INNER JOIN, faça uma busca que retorne os filmes e sua
+avaliação (rating) em ordem decrescente.
+*/
+SELECT id, title, rating
+FROM Movies
+INNER JOIN BoxOffice ON BoxOffice.movie_id = Movies.id;
