1770e948a953730491c6db9f4b289c09870977dd
Natã Elienai <nata.elienai@gmail.com>
2022-03-03 12:59:44 -0300

    feat(21.2): complete exercise 2

--- back-end/bloco-21-funcoes-sql-joins-e-normalizacao/dia-2-descomplicando-joins/exercises.sql
+++ back-end/bloco-21-funcoes-sql-joins-e-normalizacao/dia-2-descomplicando-joins/exercises.sql
@@ -7,3 +7,13 @@ internacionais (international_sales) de cada filme.
 SELECT id, title, domestic_sales, international_sales
 FROM Movies
 INNER JOIN BoxOffice ON BoxOffice.movie_id = Movies.id;
+
+/*
+2. Utilizando o INNER JOIN , faça uma busca que retorne o número de vendas para
+cada filme que possui um número maior de vendas internacionais
+(international_sales) do que vendas nacionais (domestic_sales). 
+*/
+SELECT id, title, domestic_sales, international_sales
+FROM Movies
+INNER JOIN BoxOffice ON BoxOffice.movie_id = Movies.id
+WHERE international_sales > domestic_sales;
