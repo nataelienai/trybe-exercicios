c9b6ad35eeacbd22fe3e4ef3542d52e99918b960
Natã Elienai <nata.elienai@gmail.com>
2022-03-04 19:07:54 -0300

    feat(21.x.1): union - complete exercise 4

--- back-end/bloco-21-funcoes-sql-joins-e-normalizacao/extra-1-funcoes-avancadas-do-mysql/union/exercises.sql
+++ back-end/bloco-21-funcoes-sql-joins-e-normalizacao/extra-1-funcoes-avancadas-do-mysql/union/exercises.sql
@@ -30,3 +30,15 @@ UNION
 UNION
 (SELECT first_name FROM customer LIMIT 5 OFFSET 14)
 ORDER BY first_name;
+
+/*
+4. Você quer exibir uma lista paginada com os nomes e sobrenomes de todos os
+clientes e atores do banco de dados, em ordem alfabética. Considere que a
+paginação está sendo feita de 15 em 15 resultados e que você está na 4ª página.
+Monte uma query que simule esse cenário. 
+*/
+(SELECT first_name, last_name FROM customer)
+UNION
+(SELECT first_name, last_name FROM actor)
+ORDER BY first_name, last_name
+LIMIT 15 OFFSET 45;
