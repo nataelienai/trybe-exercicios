269e6583cf1a4e15841736368c1b205628d66816
Natã Elienai <nata.elienai@gmail.com>
2022-03-04 20:58:29 -0300

    feat(21.x.1): trigger - complete exercise 3

--- back-end/bloco-21-funcoes-sql-joins-e-normalizacao/extra-1-funcoes-avancadas-do-mysql/trigger/exercises.sql
+++ back-end/bloco-21-funcoes-sql-joins-e-normalizacao/extra-1-funcoes-avancadas-do-mysql/trigger/exercises.sql
@@ -30,3 +30,18 @@ BEGIN
 		NEW.acao = 'ATUALIZAÇÃO';
 END $$
 DELIMITER ;
+
+/*
+3. Crie um TRIGGER que, a cada exclusão feita na tabela carros, envie para
+a tabela log_operacoes as informações do tipo_operacao como 'EXCLUSÃO' e a
+data_ocorrido como o momento da operação. 
+*/
+DELIMITER $$
+CREATE TRIGGER before_carros_delete
+	BEFORE DELETE ON carros
+    FOR EACH ROW
+BEGIN
+	INSERT INTO log_operacoes (tipo_operacao, data_ocorrido)
+    VALUES ('EXCLUSÃO', NOW());
+END $$
+DELIMITER ;
