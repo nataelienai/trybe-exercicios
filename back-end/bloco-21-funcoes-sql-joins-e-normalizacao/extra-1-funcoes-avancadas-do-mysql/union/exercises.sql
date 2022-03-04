afc2dd055ced557c15ee382e75be405b47f3fab0
Natã Elienai <nata.elienai@gmail.com>
2022-03-04 19:03:52 -0300

    feat(21.x.1): union - complete exercise 3

--- back-end/bloco-21-funcoes-sql-joins-e-normalizacao/extra-1-funcoes-avancadas-do-mysql/union/exercises.sql
+++ back-end/bloco-21-funcoes-sql-joins-e-normalizacao/extra-1-funcoes-avancadas-do-mysql/union/exercises.sql
@@ -11,9 +11,22 @@ UNION ALL
 
 /*
 2. Monte uma query que una os resultados das tabelas customer e actor, exibindo
-os nomes que contêm a palavra "tracy" na tabela customer e os que contêm "je" na
-tabela actor. Exiba apenas os resultados únicos. 
+os nomes que contêm a palavra "tracy" na tabela customer e os que contêm "je"
+na tabela actor. Exiba apenas os resultados únicos.
 */
 (SELECT first_name FROM customer WHERE first_name LIKE '%tracy%')
 UNION
 (SELECT first_name FROM actor WHERE first_name LIKE '%je%');
+
+/*
+3. Monte uma query que exiba a união dos cinco últimos nomes da tabela actor, o
+primeiro nome da tabela staff e cinco nomes a partir da 15ª posição da tabela
+customer. Não permita que dados repetidos sejam exibidos. Ordene os resultados
+em ordem alfabética.
+*/
+(SELECT first_name FROM actor ORDER BY first_name DESC LIMIT 5)
+UNION
+(SELECT first_name FROM staff LIMIT 1)
+UNION
+(SELECT first_name FROM customer LIMIT 5 OFFSET 14)
+ORDER BY first_name;
