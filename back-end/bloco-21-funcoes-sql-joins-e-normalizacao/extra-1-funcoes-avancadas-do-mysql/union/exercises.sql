23085017485381a20ccf30d40300a5f5ae5c020f
Natã Elienai <nata.elienai@gmail.com>
2022-03-04 18:51:59 -0300

    feat(21.x.1): union - complete exercise 2

--- back-end/bloco-21-funcoes-sql-joins-e-normalizacao/extra-1-funcoes-avancadas-do-mysql/union/exercises.sql
+++ back-end/bloco-21-funcoes-sql-joins-e-normalizacao/extra-1-funcoes-avancadas-do-mysql/union/exercises.sql
@@ -8,3 +8,12 @@ sobrenome. Seu resultado não deve excluir nenhum funcionário ao unir as tabela
 (SELECT first_name, last_name FROM staff)
 UNION ALL
 (SELECT first_name, last_name FROM actor);
+
+/*
+2. Monte uma query que una os resultados das tabelas customer e actor, exibindo
+os nomes que contêm a palavra "tracy" na tabela customer e os que contêm "je" na
+tabela actor. Exiba apenas os resultados únicos. 
+*/
+(SELECT first_name FROM customer WHERE first_name LIKE '%tracy%')
+UNION
+(SELECT first_name FROM actor WHERE first_name LIKE '%je%');
