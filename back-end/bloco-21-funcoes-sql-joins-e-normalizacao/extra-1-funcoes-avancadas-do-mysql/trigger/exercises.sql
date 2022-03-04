1bfbdfb79d436e276187a06bd540478740143453
Natã Elienai <nata.elienai@gmail.com>
2022-03-04 20:55:28 -0300

    feat(21.x.1): trigger - complete exercise 2

--- back-end/bloco-21-funcoes-sql-joins-e-normalizacao/extra-1-funcoes-avancadas-do-mysql/trigger/exercises.sql
+++ back-end/bloco-21-funcoes-sql-joins-e-normalizacao/extra-1-funcoes-avancadas-do-mysql/trigger/exercises.sql
@@ -15,3 +15,18 @@ BEGIN
 		NEW.disponivel_em_estoque = 1;
 END $$
 DELIMITER ;
+
+/*
+2. Crie um TRIGGER que, a cada atualização feita na tabela carros, defina o
+valor da coluna data_atualizacao para o momento do ocorrido e a acao para
+'ATUALIZAÇÃO'.
+*/
+DELIMITER $$
+CREATE TRIGGER before_carros_update
+	BEFORE UPDATE ON carros
+    FOR EACH ROW
+BEGIN
+	SET NEW.data_atualizacao = NOW(),
+		NEW.acao = 'ATUALIZAÇÃO';
+END $$
+DELIMITER ;
