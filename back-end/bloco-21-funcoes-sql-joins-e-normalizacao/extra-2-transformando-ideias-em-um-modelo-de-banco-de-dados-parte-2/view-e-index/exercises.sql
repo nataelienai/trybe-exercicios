a4defbe18d134c1ccc682fcedfe30d2b0e9c0c5c
Natã Elienai <nata.elienai@gmail.com>
2022-03-05 16:02:45 -0300

    feat(21.x.2): view and index: complete exercise 3

--- back-end/bloco-21-funcoes-sql-joins-e-normalizacao/extra-2-transformando-ideias-em-um-modelo-de-banco-de-dados-parte-2/view-e-index/exercises.sql
+++ back-end/bloco-21-funcoes-sql-joins-e-normalizacao/extra-2-transformando-ideias-em-um-modelo-de-banco-de-dados-parte-2/view-e-index/exercises.sql
@@ -1,10 +1,10 @@
 USE sakila;
 
 /*
- 1. Crie uma view chamada film_with_categories utilizando as tabelas category,
- film_category e film do banco de dados sakila. Essa view deve exibir o título
- do filme, o id da categoria e o nome da categoria, conforme a imagem abaixo.
- Os resultados devem ser ordenados pelo título do filme.
+1. Crie uma view chamada film_with_categories utilizando as tabelas category,
+film_category e film do banco de dados sakila. Essa view deve exibir o título
+do filme, o id da categoria e o nome da categoria. Os resultados devem ser
+ordenados pelo título do filme.
  */
 CREATE VIEW film_with_categories AS
 	SELECT f.title, c.category_id, c.name
@@ -19,8 +19,7 @@ SELECT * FROM film_with_categories;
 2. Crie uma view chamada film_info utilizando as tabelas actor, film_actor e
 film do banco de dados sakila. Sua view deve exibir o actor_id, o nome completo
 do ator ou da atriz em uma coluna com o ALIAS actor e o título dos filmes. Os
-resultados devem ser ordenados pelos nomes de atores e atrizes. Use a imagem a
-seguir como referência.
+resultados devem ser ordenados pelos nomes de atores e atrizes.
 */
 CREATE VIEW film_info AS
 	SELECT a.actor_id, CONCAT(a.first_name, ' ', a.last_name) AS actor, f.title
@@ -30,3 +29,17 @@ CREATE VIEW film_info AS
     ORDER BY actor;
 
 SELECT * FROM film_info;
+
+/*
+3. Crie uma view chamada address_info que faça uso das tabelas address e city
+do banco de dados sakila. Sua view deve exibir o address_id, o address,
+o district, o city_id e a city. Os resultados devem ser ordenados pelo nome das
+cidades.
+*/
+CREATE VIEW address_info AS
+	SELECT a.address_id, a.address, a.district, c.city_id, c.city
+    FROM address a
+    INNER JOIN city c ON a.city_id = c.city_id
+    ORDER BY c.city;
+
+SELECT * FROM address_info;
