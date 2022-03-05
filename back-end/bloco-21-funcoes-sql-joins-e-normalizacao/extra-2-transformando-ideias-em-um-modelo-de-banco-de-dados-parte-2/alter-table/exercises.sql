0bd2701101a5857915257f534996d60573b4ad11
Natã Elienai <nata.elienai@gmail.com>
2022-03-05 16:46:42 -0300

    feat(21.x.2): alter table - complete exercise 3

--- back-end/bloco-21-funcoes-sql-joins-e-normalizacao/extra-2-transformando-ideias-em-um-modelo-de-banco-de-dados-parte-2/alter-table/exercises.sql
+++ back-end/bloco-21-funcoes-sql-joins-e-normalizacao/extra-2-transformando-ideias-em-um-modelo-de-banco-de-dados-parte-2/alter-table/exercises.sql
@@ -11,3 +11,9 @@ ALTER TABLE locations CHANGE STREET_ADDRESS ADDRESS VARCHAR(40);
 region_name para region, mantendo o mesmo tipo e tamanho de dados.
 */
 ALTER TABLE regions CHANGE REGION_NAME REGION VARCHAR(25);
+
+/*
+3. Escreva uma query SQL para alterar na tabela countries o nome da coluna
+country_name para country, mantendo o mesmo tipo e tamanho de dados.
+*/
+ALTER TABLE countries CHANGE COUNTRY_NAME COUNTRY VARCHAR(40);
