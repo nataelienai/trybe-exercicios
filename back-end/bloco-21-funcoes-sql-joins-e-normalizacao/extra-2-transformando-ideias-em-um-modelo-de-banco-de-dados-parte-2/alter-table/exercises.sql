cd9a9e7b4ca96bb11a2a509953a5fbb14cb75012
Natã Elienai <nata.elienai@gmail.com>
2022-03-05 16:43:13 -0300

    feat(21.x.2): alter table - complete exercise 2

--- back-end/bloco-21-funcoes-sql-joins-e-normalizacao/extra-2-transformando-ideias-em-um-modelo-de-banco-de-dados-parte-2/alter-table/exercises.sql
+++ back-end/bloco-21-funcoes-sql-joins-e-normalizacao/extra-2-transformando-ideias-em-um-modelo-de-banco-de-dados-parte-2/alter-table/exercises.sql
@@ -5,3 +5,9 @@ USE hr;
 street_address para address, mantendo o mesmo tipo e tamanho de dados.
 */
 ALTER TABLE locations CHANGE STREET_ADDRESS ADDRESS VARCHAR(40);
+
+/*
+2. Escreva uma query SQL para alterar na tabela regions o nome da coluna
+region_name para region, mantendo o mesmo tipo e tamanho de dados.
+*/
+ALTER TABLE regions CHANGE REGION_NAME REGION VARCHAR(25);
