02b9eb536002e02cbb1535a917576b58ed703bde
Natã Elienai <nata.elienai@gmail.com>
2022-03-05 16:06:09 -0300

    feat(21.x.2): view and index: complete exercise 4

--- back-end/bloco-21-funcoes-sql-joins-e-normalizacao/extra-2-transformando-ideias-em-um-modelo-de-banco-de-dados-parte-2/view-e-index/exercises.sql
+++ back-end/bloco-21-funcoes-sql-joins-e-normalizacao/extra-2-transformando-ideias-em-um-modelo-de-banco-de-dados-parte-2/view-e-index/exercises.sql
@@ -43,3 +43,15 @@ CREATE VIEW address_info AS
     ORDER BY c.city;
 
 SELECT * FROM address_info;
+
+/*
+4. Crie uma view chamada movies_languages, usando as tabelas film e language do
+banco de dados sakila. Sua view deve exibir o título do filme, o id do idioma e
+o idioma do filme, como na imagem a seguir.
+*/
+CREATE VIEW movies_languages AS
+	SELECT f.title, l.language_id, l.name
+    FROM film f
+    INNER JOIN language l ON f.language_id = l.language_id;
+
+SELECT * FROM movies_languages;
