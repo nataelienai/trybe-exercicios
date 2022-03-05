25fa2a9f69e12b0ed8ba885167e4485a1ae993e4
Natã Elienai <nata.elienai@gmail.com>
2022-03-05 16:31:41 -0300

    feat(21.x.2): view and index: complete exercise 6

--- back-end/bloco-21-funcoes-sql-joins-e-normalizacao/extra-2-transformando-ideias-em-um-modelo-de-banco-de-dados-parte-2/view-e-index/exercises.sql
+++ back-end/bloco-21-funcoes-sql-joins-e-normalizacao/extra-2-transformando-ideias-em-um-modelo-de-banco-de-dados-parte-2/view-e-index/exercises.sql
@@ -75,3 +75,23 @@ DROP INDEX category_name_index ON category;
 SELECT *
 FROM sakila.category
 WHERE name LIKE '%action%';
+
+/*
+6. Verifique o impacto de um INDEX na tabela address (banco de dados sakila)
+adicionando-o na coluna postal_code. Após ter adicionado o índice, mensure o
+custo da query utilizando o execution plan. Após ter criado e mensurado o
+custo da query, exclua o índice e mensure novamente esse custo. 
+*/
+CREATE INDEX address_postal_code_index ON address(postal_code);
+
+# Query cost: 0.35
+SELECT *
+FROM sakila.address
+WHERE postal_code = '36693';
+
+DROP INDEX address_postal_code_index ON address;
+
+# Query cost: 61.80
+SELECT *
+FROM sakila.address
+WHERE postal_code = '36693';
