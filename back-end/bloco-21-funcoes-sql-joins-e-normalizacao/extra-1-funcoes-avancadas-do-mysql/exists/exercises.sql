2bf34859f37df442e0f9809162cea1088f964cb0
Natã Elienai <nata.elienai@gmail.com>
2022-03-04 21:05:20 -0300

    feat(21.x.1): exists - complete exercise 2

--- back-end/bloco-21-funcoes-sql-joins-e-normalizacao/extra-1-funcoes-avancadas-do-mysql/exists/exercises.sql
+++ back-end/bloco-21-funcoes-sql-joins-e-normalizacao/extra-1-funcoes-avancadas-do-mysql/exists/exercises.sql
@@ -10,3 +10,14 @@ WHERE id NOT IN (
 	SELECT book_id
     FROM Books_Lent
 );
+
+/*
+2. Usando o EXISTS na tabela books_lent e books , exiba o id e título dos
+livros estão atualmente emprestados e que contêm a palavra "lost" no título.
+*/
+SELECT id, title
+FROM Books
+WHERE title LIKE '%lost%' AND id IN (
+	SELECT book_id
+    FROM Books_Lent
+);
