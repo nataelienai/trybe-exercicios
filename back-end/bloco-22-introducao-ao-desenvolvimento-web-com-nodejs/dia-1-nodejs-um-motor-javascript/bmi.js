4602a1ed83a837424e3dbea16d9a4653d4433299
Natã Elienai <nata.elienai@gmail.com>
2022-03-11 21:05:12 -0300

    feat(22.1): create a script to calculate a person's BMI

---
+++ back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-1-nodejs-um-motor-javascript/bmi.js
@@ -0,0 +1,5 @@
+
+const weight = 83;
+const height = 1.80;
+const bmi = weight / (height * height);
+console.log(`The BMI calculated with a weight of ${weight} kg and a height of ${height} meters is ${bmi}`);
