86df5234ebac6210059a6d9912f2e72ecf2f35f2
Natã Elienai <nata.elienai@gmail.com>
2022-03-11 21:42:03 -0300

    feat(22.1): add user's BMI category to BMI calculation result

--- back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-1-nodejs-um-motor-javascript/bmi.js
+++ back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-1-nodejs-um-motor-javascript/bmi.js
@@ -1,5 +1,22 @@
 const readlineSync = require('readline-sync');
+
 const weight = readlineSync.questionFloat('How much do you weigh? (in kg) ');
 const height = readlineSync.questionFloat('How tall are you? (in meters) ');
+
 const bmi = weight / (height * height);
+let category;
+if (bmi < 18.5) {
+  category = 'Underweight';
+} else if (bmi < 25) {
+  category = 'Normal';
+} else if (bmi < 30) {
+  category = 'Overweight';
+} else if (bmi < 35) {
+  category = 'Obese class I';
+} else if (bmi < 40) {
+  category = 'Obese class II';
+} else {
+  category = 'Obese class III';
+}
 console.log(`Your BMI is ${bmi.toFixed(2)}`);
+console.log(`Your weight is in the ${category} category`);
