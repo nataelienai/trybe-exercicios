291a37414a5ccbd0c8e83bf0febcd3521ec2e328
Natã Elienai <nata.elienai@gmail.com>
2022-03-12 15:48:47 -0300

    feat(22.1): create a script to select which script to run

---
+++ back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-1-nodejs-um-motor-javascript/index.js
@@ -0,0 +1,17 @@
+const readlineSync = require('readline-sync');
+
+function main() {
+  const scripts = {
+    'Body Mass Index (BMI) Calculator': './bmi',
+    'Average Speed Calculator': './speed',
+    'Guessing game': './guessing',
+  };
+  const scriptNames = Object.keys(scripts);
+  const option = readlineSync.keyInSelect(scriptNames, 'Which script should be run?');
+  const chosenScript = scriptNames[option];
+
+  if (!chosenScript) return;
+  require(scripts[chosenScript]);
+}
+
+main();
