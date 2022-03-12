904fff45b56c4c1b3b393dd687c764efa5df2efd
Natã Elienai <nata.elienai@gmail.com>
2022-03-12 12:15:21 -0300

    refactor(22.1): separate BMI global scope code into functions

--- back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-1-nodejs-um-motor-javascript/bmi.js
+++ back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-1-nodejs-um-motor-javascript/bmi.js
@@ -1,22 +1,33 @@
 const readlineSync = require('readline-sync');
 
-const weight = readlineSync.questionFloat('How much do you weigh? (in kg) ');
-const height = readlineSync.questionFloat('How tall are you? (in meters) ');
-
-const bmi = weight / (height * height);
-let category;
-if (bmi < 18.5) {
-  category = 'Underweight';
-} else if (bmi < 25) {
-  category = 'Normal';
-} else if (bmi < 30) {
-  category = 'Overweight';
-} else if (bmi < 35) {
-  category = 'Obese class I';
-} else if (bmi < 40) {
-  category = 'Obese class II';
-} else {
-  category = 'Obese class III';
+function calculateBMI(weight, height) {
+  return weight / (height * height);
 }
-console.log(`Your BMI is ${bmi.toFixed(2)}`);
-console.log(`Your weight is in the ${category} category`);
+
+function getWeightStatus(bmi) {
+  if (bmi < 18.5) {
+    return 'Underweight';
+  } else if (bmi < 25) {
+    return 'Normal';
+  } else if (bmi < 30) {
+    return 'Overweight';
+  } else if (bmi < 35) {
+    return 'Obese class I';
+  } else if (bmi < 40) {
+    return 'Obese class II';
+  }
+  return 'Obese class III';
+}
+
+function main() {
+  const weight = readlineSync.questionFloat('How much do you weigh? (in kg) ');
+  const height = readlineSync.questionFloat('How tall are you? (in meters) ');
+
+  const bmi = calculateBMI(weight, height);
+  const category = getWeightStatus(bmi);
+
+  console.log(`Your BMI is ${bmi.toFixed(2)}`);
+  console.log(`Your weight is in the ${category} category`);
+}
+
+main();
