0c633c818d624e1f4c34ebbc46f59a43e04b1aa6
Natã Elienai <nata.elienai@gmail.com>
2022-03-12 13:28:53 -0300

    refactor(22.1): separate guessing global scope code into functions

--- back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-1-nodejs-um-motor-javascript/guessing.js
+++ back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-1-nodejs-um-motor-javascript/guessing.js
@@ -1,10 +1,18 @@
 const readlineSync = require('readline-sync');
 
-const drawnNumber = Math.floor(Math.random() * 11);
-const guessedNumber = readlineSync.questionInt('Guess a number from 0 to 10: ');
-
-if (drawnNumber === guessedNumber) {
-  console.log('Congratulations, you got it right!');
-} else {
-  console.log(`Oops, not this time. The correct number was ${drawnNumber}`);
+function getRandomInteger(upperLimit) {
+  return Math.floor(Math.random() * (upperLimit + 1));
 }
+
+function main() {
+  const guessedNumber = readlineSync.questionInt('Guess a number from 0 to 10: ');
+  const drawnNumber = getRandomInteger(10);
+
+  if (drawnNumber === guessedNumber) {
+    console.log('Congratulations, you got it right!');
+  } else {
+    console.log(`Oops, not this time. The correct number was ${drawnNumber}`);
+  }
+}
+
+main();
