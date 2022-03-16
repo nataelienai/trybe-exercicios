13fa0936e80a7dfbfbba94df21619d8aff252895
Natã Elienai <nata.elienai@gmail.com>
2022-03-16 15:55:06 -0300

    test(22.3): check that getNumberPolarity returns the correct polarity

--- back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-3-mocha-chai-e-sinon-testes-de-back-end-com-nodejs/tests/getNumberPolarity.test.js
+++ back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-3-mocha-chai-e-sinon-testes-de-back-end-com-nodejs/tests/getNumberPolarity.test.js
@@ -15,8 +15,22 @@ describe('getNumberPolarity', () => {
   describe('When given a number', () => {
     it('returns a string', () => {
       const result = getNumberPolarity(5);
-
       expect(result).to.be.a('string');
+    });
+
+    it('returns \'positive\' for positive numbers', () => {
+      const result = getNumberPolarity(7);
+      expect(result).to.equal('positive');
+    });
+
+    it('returns \'negative\' for negative numbers', () => {
+      const result = getNumberPolarity(-5);
+      expect(result).to.equal('negative');
+    });
+
+    it('returns \'neutral\' for 0', () => {
+      const result = getNumberPolarity(0);
+      expect(result).to.equal('neutral');
     });
   });
 });
