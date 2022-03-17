7730bcff667c67dacb6f184ebd045d7ec9da102c
Natã Elienai <nata.elienai@gmail.com>
2022-03-17 16:24:20 -0300

    feat(22.4): create route POST /hello

--- back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-4-express-http-com-nodejs/simpleApi.js
+++ back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-4-express-http-com-nodejs/simpleApi.js
@@ -2,9 +2,15 @@ const express = require('express');
 
 const PORT = 8080;
 const app = express();
+app.use(express.json());
 
 app.get('/ping', (req, res) => {
   res.status(200).json({ message: 'pong' });
+});
+
+app.post('/hello', (req, res) => {
+  const { name } = req.body;
+  res.status(201).json({ message: `Hello, ${name}!` });
 });
 
 app.listen(PORT, () => console.log(`Express is listening on port ${PORT}`));
