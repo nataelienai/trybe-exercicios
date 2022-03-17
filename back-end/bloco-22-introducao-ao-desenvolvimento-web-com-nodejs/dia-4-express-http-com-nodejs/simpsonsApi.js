2c256a3c84f5e18ef8e928ea95dac852699c403d
Natã Elienai <nata.elienai@gmail.com>
2022-03-17 17:14:45 -0300

    feat(22.4): create endpoint GET /simpsons/:id

--- back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-4-express-http-com-nodejs/simpsonsApi.js
+++ back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-4-express-http-com-nodejs/simpsonsApi.js
@@ -4,11 +4,38 @@ const fs = require('fs').promises;
 const PORT = 3001;
 const app = express();
 
+async function getSimpsons() {
+  const fileContent = await fs.readFile('./simpsons.json', 'utf-8');
+  const simpsons = JSON.parse(fileContent);
+  return simpsons;
+}
+
+async function getSimpsonById(id) {
+  const simpsons = await getSimpsons();
+  const wantedSimpson = simpsons.find((simpson) => (
+    parseInt(simpson.id) === parseInt(id)
+  ));
+  return wantedSimpson;
+}
+
 app.get('/simpsons', async (req, res) => {
   try {
-    const fileContent = await fs.readFile('./simpsons.json', 'utf-8');
-    const simpsons = JSON.parse(fileContent);
+    const simpsons = await getSimpsons();
     res.status(200).json(simpsons);
+  } catch {
+    res.status(500).end();
+  }
+});
+
+app.get('/simpsons/:id', async (req, res) => {
+  const { id } = req.params;
+  try {
+    const simpson = await getSimpsonById(id);
+
+    if (!simpson) {
+      return res.status(404).json({ message: 'simpson not found!' });
+    }
+    res.status(200).json(simpson);
   } catch {
     res.status(500).end();
   }
