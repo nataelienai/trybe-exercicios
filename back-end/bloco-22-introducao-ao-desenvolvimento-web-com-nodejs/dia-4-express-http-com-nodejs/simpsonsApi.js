a9ca21bbb3e7ce656718cdfa0e4d042bf323d80d
Natã Elienai <nata.elienai@gmail.com>
2022-03-17 18:12:52 -0300

    feat(22.4): create endpoint POST /simpsons

--- back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-4-express-http-com-nodejs/simpsonsApi.js
+++ back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-4-express-http-com-nodejs/simpsonsApi.js
@@ -4,6 +4,8 @@ const fs = require('fs').promises;
 const PORT = 3001;
 const app = express();
 
+app.use(express.json());
+
 async function getSimpsons() {
   const fileContent = await fs.readFile('./simpsons.json', 'utf-8');
   const simpsons = JSON.parse(fileContent);
@@ -16,6 +18,21 @@ async function getSimpsonById(id) {
     parseInt(simpson.id) === parseInt(id)
   ));
   return wantedSimpson;
+}
+
+async function isSimpsonIdTaken(id) {
+  const simpsons = await getSimpsons();
+  const isTaken = simpsons.some((simpson) => (
+    parseInt(simpson.id) === parseInt(id)
+  ));
+  return isTaken;
+}
+
+async function addSimpson(simpson) {
+  const simpsons = await getSimpsons();
+  simpsons.push(simpson);
+
+  await fs.writeFile('./simpsons.json', JSON.stringify(simpsons, null, 2));
 }
 
 app.get('/simpsons', async (req, res) => {
@@ -36,6 +53,21 @@ app.get('/simpsons/:id', async (req, res) => {
       return res.status(404).json({ message: 'simpson not found!' });
     }
     res.status(200).json(simpson);
+  } catch {
+    res.status(500).end();
+  }
+});
+
+app.post('/simpsons', async (req, res) => {
+  const { id, name } = req.body;
+
+  try {
+    const isIdTaken = await isSimpsonIdTaken(id);
+    if (isIdTaken) {
+      return res.status(409).json({ message: 'id already exists!' });
+    }
+    addSimpson({ id, name });
+    res.status(204).end();
   } catch {
     res.status(500).end();
   }
