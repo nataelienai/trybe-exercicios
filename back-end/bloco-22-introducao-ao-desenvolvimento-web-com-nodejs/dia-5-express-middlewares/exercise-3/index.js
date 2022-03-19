de9857edfe840e343324dbc70a380b9f49a87322
Natã Elienai <nata.elienai@gmail.com>
2022-03-19 12:58:15 -0300

    feat(22.5): add error handling to postsRouter

--- back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-5-express-middlewares/exercise-3/index.js
+++ back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-5-express-middlewares/exercise-3/index.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const postsRouter = require('./postsRouter');
 const routeNotFoundMiddleware = require('./routeNotFoundMiddleware');
+const errorMiddleware = require('./errorMiddleware');
 
 const app = express();
 
 app.use(express.json());
 app.use('/posts', postsRouter);
 app.all('*', routeNotFoundMiddleware);
+app.use(errorMiddleware);
 
 app.listen(3000, () => console.log('Express listening on port 3000'));
--- back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-5-express-middlewares/exercise-3/postsRouter.js
+++ back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-5-express-middlewares/exercise-3/postsRouter.js
@@ -1,11 +1,11 @@
 const express = require('express');
-const { route } = require('express/lib/router');
+const rescue = require('express-rescue');
 
 const router = express.Router();
 
 const posts = [{ id: 1, content: 'I know that I know nothing' }];
 
-router.get('/:id', (req, res) => {
+router.get('/:id', rescue((req, res) => {
   const { id } = req.params;
   const post = posts.find((post) => post.id === Number(id));
 
@@ -13,10 +13,10 @@ router.get('/:id', (req, res) => {
     return res.status(404).json({ message: 'post not found' });
   }
   res.status(200).json(post);
-});
+}));
 
-router.get('/', (_req, res) => {
+router.get('/', rescue((_req, res) => {
   res.status(200).json({ posts });
-})
+}));
 
 module.exports = router;
