bd7e7b6f8db8d204b83a7985cb9b30e00ca3f613
Natã Elienai <nata.elienai@gmail.com>
2022-03-19 11:56:00 -0300

    feat(22.5): add return data to route GET /btc/price

--- back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-5-express-middlewares/exercise-2/index.js
+++ back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-5-express-middlewares/exercise-2/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const axios = require('axios').default;
 
 const app = express();
 
@@ -13,13 +14,14 @@ function isTokenValid(token) {
   return token && token.length === 12 && hasOnlyLettersAndNumbers(token);
 }
 
-app.get('/btc/price', (req, res) => {
+app.get('/btc/price', async (req, res) => {
   const token = req.headers.authorization;
 
   if (!isTokenValid(token)) {
     return res.status(401).json({ message: 'invalid token' });
   }
-  res.status(200).json({});
+  const response = await axios.get('https://api.coindesk.com/v1/bpi/currentprice/BTC.json');
+  res.status(200).json(response.data);
 });
 
 app.listen(3000, () => console.log('Express listening on port 3000'));
