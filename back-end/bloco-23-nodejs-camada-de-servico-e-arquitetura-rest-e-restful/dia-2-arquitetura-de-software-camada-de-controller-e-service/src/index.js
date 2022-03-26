16569e8d37c6fd98453083042691fd5c9b873be7
Natã Elienai <nata.elienai@gmail.com>
2022-03-26 13:43:42 -0300

    feat(23.2): create GET /ping endpoint to check API status

--- back-end/bloco-23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-2-arquitetura-de-software-camada-de-controller-e-service/src/index.js
+++ back-end/bloco-23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-2-arquitetura-de-software-camada-de-controller-e-service/src/index.js
@@ -5,4 +5,8 @@ const PORT = 3000;
 
 app.use(express.json());
 
+app.get('/ping', (_req, res) => {
+  res.status(200).json({ message: 'pong!' });
+});
+
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
