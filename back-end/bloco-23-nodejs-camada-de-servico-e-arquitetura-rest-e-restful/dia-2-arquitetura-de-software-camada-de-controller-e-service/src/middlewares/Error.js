e6ce41071047e33ff8f08d099fbf916b8cdd41f3
Natã Elienai <nata.elienai@gmail.com>
2022-03-28 11:30:42 -0300

    feat(23.2): add a create function to Cep service with CEP formatting and validation

--- back-end/bloco-23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-2-arquitetura-de-software-camada-de-controller-e-service/src/middlewares/Error.js
+++ back-end/bloco-23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-2-arquitetura-de-software-camada-de-controller-e-service/src/middlewares/Error.js
@@ -2,12 +2,13 @@ module.exports = (err, _req, res, _next) => {
   const statusByErrorCode = {
     invalidData: 400,
     notFound: 404,
+    alreadyExists: 409,
   };
 
   const status = statusByErrorCode[err.code] || 500;
 
   if (status === 500) {
-    return res.status(status).end();
+    return res.status(status).json(err.message);
   }
 
   res.status(status).json({ error: err });
--- back-end/bloco-23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-2-arquitetura-de-software-camada-de-controller-e-service/src/services/Cep.js
+++ back-end/bloco-23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-2-arquitetura-de-software-camada-de-controller-e-service/src/services/Cep.js
@@ -1,21 +1,84 @@
 const Cep = require('../models/Cep');
+const Joi = require('joi');
 
 const isValid = (cep) => {
-  const cepRegex = /\d{5}-?\d{3}/;
+  const cepRegex = /^\d{5}-?\d{3}$/;
   return cepRegex.test(cep);
 };
+
+const validateDetails = (cepDetails) => {
+  const schema = Joi.object({
+    cep: Joi.string().not().empty().required(),
+    logradouro: Joi.string().not().empty().required(),
+    bairro: Joi.string().not().empty().required(),
+    localidade: Joi.string().not().empty().required(),
+    uf: Joi.string().not().empty().required(),
+  });
+
+  return schema.validate(cepDetails);
+};
+
+const exists = async (cep) => {
+  const cepDetails = await Cep.getDetails(cep);
+  return Boolean(cepDetails);
+};
+
+const removeDashFromCep = (cep) => cep.replace('-', '');
+
+const formatCep = (cep) => cep.replace(/(\d{5})(\d{3})/, '$1-$2');
+
+const removeDashFromCepInDetails = ({ cep, logradouro, bairro, localidade, uf }) => ({
+  cep: removeDashFromCep(cep),
+  logradouro,
+  bairro,
+  localidade,
+  uf
+});
+
+const formatCepInDetails = ({ cep, logradouro, bairro, localidade, uf }) => ({
+  cep: formatCep(cep),
+  logradouro,
+  bairro,
+  localidade,
+  uf
+});
 
 const getDetails = async (cep) => {
   if (!isValid(cep)) {
     return { error: { code: 'invalidData', message: 'CEP inválido' } };
   }
-  const cepDetails = await Cep.getDetails(cep);
+
+  const unformattedCep = removeDashFromCep(cep);
+  const cepDetails = await Cep.getDetails(unformattedCep);
+
   if (!cepDetails) {
     return { error: { code: 'notFound', message: 'CEP não encontrado' } };
   }
-  return { data: cepDetails };
+  return { data: formatCepInDetails(cepDetails) };
+};
+
+const create = async (cepDetails) => {
+  const { error } = validateDetails(cepDetails);
+
+  if (error) {
+    return { error: { code: 'invalidData', message: error.details[0].message } };
+  }
+  if (!isValid(cepDetails.cep)) {
+    return { error: { code: 'invalidData', message: 'CEP inválido' } };
+  }
+
+  const unformattedCepDetails = removeDashFromCepInDetails(cepDetails);
+  const cepExists = await exists(unformattedCepDetails.cep);
+
+  if (cepExists) {
+    return { error: { code: 'alreadyExists', message: 'CEP já existente' } };
+  }
+
+  const createdCepDetails = await Cep.create(unformattedCepDetails);
+  return { data: formatCepInDetails(createdCepDetails) };
 };
 
 module.exports = {
   getDetails,
+  create,
 };
