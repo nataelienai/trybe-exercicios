28100063511bbf1bb87823bed95c43f26e9e3bc8
Natã Elienai <nata.elienai@gmail.com>
2022-03-28 11:31:37 -0300

    feat(23.2): add a create function to the Cep controller

--- back-end/bloco-23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-2-arquitetura-de-software-camada-de-controller-e-service/src/controllers/Cep.js
+++ back-end/bloco-23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-2-arquitetura-de-software-camada-de-controller-e-service/src/controllers/Cep.js
@@ -8,6 +8,15 @@ const getDetails = async (req, res, next) => {
   res.status(200).json(data);
 };
 
+const create = async (req, res, next) => {
+  const { cep, logradouro, bairro, localidade, uf } = req.body;
+  const { error, data } = await Cep.create({ cep, logradouro, bairro, localidade, uf });
+  
+  if (error) return next(error);
+  res.status(201).json(data);
+};
+
 module.exports = {
   getDetails,
+  create,
 };
