c83d5678e472e47618cfc26a84e2aeb75b712eb6
Natã Elienai <nata.elienai@gmail.com>
2022-03-28 11:23:09 -0300

    feat(23.2): add a create function to the Cep model

--- back-end/bloco-23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-2-arquitetura-de-software-camada-de-controller-e-service/src/models/Cep.js
+++ back-end/bloco-23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/dia-2-arquitetura-de-software-camada-de-controller-e-service/src/models/Cep.js
@@ -7,6 +7,15 @@ const getDetails = async (cep) => {
   return cepDetails[0];
 };
 
+const create = async ({ cep, logradouro, bairro, localidade, uf }) => {
+  await connection.execute(
+    'INSERT INTO ceps (cep, logradouro, bairro, localidade, uf) VALUES (?, ?, ?, ?, ?)',
+    [cep, logradouro, bairro, localidade, uf],
+  );
+  return { cep, logradouro, bairro, localidade, uf };
+};
+
 module.exports = {
   getDetails,
+  create,
 };
