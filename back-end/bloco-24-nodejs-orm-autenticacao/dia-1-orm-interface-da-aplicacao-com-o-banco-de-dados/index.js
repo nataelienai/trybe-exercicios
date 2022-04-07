7bab510216189af7586b52990937647e8fa3b183
Natã Elienai <nata.elienai@gmail.com>
2022-04-06 21:07:35 -0300

    feat: add book listing via /books endpoint

--- back-end/bloco-24-nodejs-orm-autenticacao/dia-1-orm-interface-da-aplicacao-com-o-banco-de-dados/index.js
+++ back-end/bloco-24-nodejs-orm-autenticacao/dia-1-orm-interface-da-aplicacao-com-o-banco-de-dados/index.js
@@ -1,9 +1,20 @@
 require('dotenv').config();
 const express = require('express');
+const { Book } = require('./src/models');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
+
+app.get('/books', async (req, res) => {
+  try {
+    const books = await Book.findAll();
+    res.status(200).json(books);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Algo deu errado' });
+  }
+});
 
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
