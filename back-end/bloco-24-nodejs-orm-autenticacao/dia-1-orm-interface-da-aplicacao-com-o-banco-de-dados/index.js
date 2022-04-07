be2c88658ab61823601aacb37b7f8e3fe9637f1c
Natã Elienai <nata.elienai@gmail.com>
2022-04-06 22:48:18 -0300

    feat: add book update via /book/:id endpoint

--- back-end/bloco-24-nodejs-orm-autenticacao/dia-1-orm-interface-da-aplicacao-com-o-banco-de-dados/index.js
+++ back-end/bloco-24-nodejs-orm-autenticacao/dia-1-orm-interface-da-aplicacao-com-o-banco-de-dados/index.js
@@ -43,4 +43,20 @@ app.post('/book', async (req, res) => {
   }
 });
 
+app.post('/book/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { title, author, pageQuantity } = req.body;
+    const result = await Book.update(
+      { title, author, pageQuantity },
+      { where: { id } }
+    );
+
+    res.status(200).json(result);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Algo deu errado' });
+  }
+});
+
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
