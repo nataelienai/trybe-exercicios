7f959b287f41ffc55c9d27ed5254ee72d95fa4a8
Natã Elienai <nata.elienai@gmail.com>
2022-04-07 12:58:25 -0300

    feat: add a seeder to populate the books table

---
+++ back-end/bloco-24-nodejs-orm-autenticacao/dia-1-orm-interface-da-aplicacao-com-o-banco-de-dados/src/seeders/20220407154305-books.js
@@ -0,0 +1,29 @@
+'use strict';
+
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    return queryInterface.bulkInsert('Books', [
+      {
+        title: 'The Fellowship of the Ring',
+        author: 'J. R. R. Tolkien',
+        page_quantity: 423,
+        created_at: Sequelize.literal('CURRENT_TIMESTAMP'),
+      },
+      {
+        title: 'Harry Potter and the Philosopher\'s Stone',
+        author: 'J. K. Rowling',
+        created_at: Sequelize.literal('CURRENT_TIMESTAMP'),
+      },
+      {
+        title: 'The Hobbit, or There and Back Again',
+        author: 'J. R. R. Tolkien',
+        page_quantity: 251,
+        created_at: Sequelize.literal('CURRENT_TIMESTAMP'),
+      },
+    ]);
+  },
+
+  async down (queryInterface, Sequelize) {
+    return queryInterface.bulkDelete('Books', null, {});
+  }
+};
