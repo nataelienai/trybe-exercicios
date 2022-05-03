0150fd87901cf0b3fbd758473ae5e781b10b28a1
Natã Elienai <nata.elienai@gmail.com>
2022-05-03 00:06:03 -0300

    feat: add exercises from day 24.2

--- back-end/bloco-24-nodejs-orm-autenticacao/dia-2-orm-associations/index.js
+++ back-end/bloco-24-nodejs-orm-autenticacao/dia-2-orm-associations/index.js
@@ -1,10 +1,13 @@
 const express = require('express');
+const PatientController = require('./src/controllers/Patient');
 
 const app = express();
 const PORT = 3000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+app.get('/patients', PatientController.findAll);
 
 app.listen(PORT, () => {
   console.log(`Port: ${PORT}`);
---
+++ back-end/bloco-24-nodejs-orm-autenticacao/dia-2-orm-associations/src/models/PatientSurgery.js
@@ -0,0 +1,24 @@
+module.exports = (sequelize, _DataTypes) => {
+  const PatientSurgery = sequelize.define('PatientSurgery', {}, {
+    timestamps: false,
+    tableName: 'Patient_surgeries',
+  });
+
+  PatientSurgery.associate = (models) => {
+    models.Patient.belongsToMany(models.Surgery, {
+      as: 'surgeries',
+      through: PatientSurgery,
+      foreignKey: 'patient_id',
+      otherKey: 'surgery_id',
+    });
+
+    models.Surgery.belongsToMany(models.Patient, {
+      as: 'patients',
+      through: PatientSurgery,
+      foreignKey: 'surgery_id',
+      otherKey: 'patient_id',
+    });
+  }
+
+  return PatientSurgery;
+};
