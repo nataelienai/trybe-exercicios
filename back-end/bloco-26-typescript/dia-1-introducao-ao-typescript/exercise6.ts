ea2f49e1df8fae16c5e984b5370c20c9e5f7942c
Natã Elienai <nata.elienai@gmail.com>
2022-05-09 20:08:23 -0300

    feat: add an execution function to exercises from 5 to 9

--- back-end/bloco-26-typescript/dia-1-introducao-ao-typescript/exercise6.ts
+++ back-end/bloco-26-typescript/dia-1-introducao-ao-typescript/exercise6.ts
@@ -1,3 +1,5 @@
+import readline from 'readline-sync';
+
 export enum Units {
   KILOGRAM = 1000,
   HECTOGRAM = 100,
@@ -13,3 +15,20 @@ type Unit = keyof typeof Units;
 export function convert(value: number, baseUnit: Unit, conversionUnit: Unit) {
   return value * (Units[baseUnit] / Units[conversionUnit]);
 }
+
+function exec() {
+  const value = readline.questionInt('Value: ');
+
+  const baseUnit = readline.question('From unit: ', {
+    limit: Object.values(Units).filter((key) => typeof key === 'string')
+  }).toUpperCase();
+
+  const conversionUnit = readline.question('To unit: ', {
+    limit: Object.values(Units).filter((key) => typeof key === 'string')
+  }).toUpperCase();
+
+  const convertedValue = convert(value, baseUnit as Unit, conversionUnit as Unit);
+  console.log(`${value} ${baseUnit} is equal to ${convertedValue} ${conversionUnit}`);
+}
+
+exec();
--- back-end/bloco-26-typescript/dia-1-introducao-ao-typescript/exercise9.ts
+++ back-end/bloco-26-typescript/dia-1-introducao-ao-typescript/exercise9.ts
@@ -1,3 +1,5 @@
+import readline from 'readline-sync';
+
 export enum Units {
   CUBIC_KILOMETER = 10 ** 9,
   CUBIC_HECTOMETER = 10 ** 6,
@@ -13,3 +15,20 @@ type Unit = keyof typeof Units;
 export function convert(value: number, baseUnit: Unit, conversionUnit: Unit) {
   return value * (Units[baseUnit] / Units[conversionUnit]);
 }
+
+function exec() {
+  const value = readline.questionInt('Value: ');
+
+  const baseUnit = readline.question('From unit: ', {
+    limit: Object.values(Units).filter((key) => typeof key === 'string')
+  }).toUpperCase();
+
+  const conversionUnit = readline.question('To unit: ', {
+    limit: Object.values(Units).filter((key) => typeof key === 'string')
+  }).toUpperCase();
+
+  const convertedValue = convert(value, baseUnit as Unit, conversionUnit as Unit);
+  console.log(`${value} ${baseUnit} is equal to ${convertedValue} ${conversionUnit}`);
+}
+
+exec();
