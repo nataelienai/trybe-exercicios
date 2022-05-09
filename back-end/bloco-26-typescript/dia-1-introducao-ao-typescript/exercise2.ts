e2ec5484dffe2712e4b52850d2c08e126b2eae6b
Natã Elienai <nata.elienai@gmail.com>
2022-05-09 16:02:33 -0300

    fix: add values to enums in exercises 2 and 3

--- back-end/bloco-26-typescript/dia-1-introducao-ao-typescript/exercise2.ts
+++ back-end/bloco-26-typescript/dia-1-introducao-ao-typescript/exercise2.ts
@@ -1,15 +1,15 @@
 // Crie uma enum que represente os meses do ano.
 export enum MonthsOfTheYear {
-  January,
-  February,
-  March,
-  April,
-  May,
-  June,
-  July,
-  August,
-  September,
-  October,
-  November,
-  December
+  JANUARY = 'January',
+  FEBRUARY = 'February',
+  MARCH = 'March',
+  APRIL = 'April',
+  MAY = 'May',
+  JUNE ='June',
+  JULY = 'July',
+  AUGUST = 'August',
+  SEPTEMBER = 'September',
+  OCTOBER = 'October',
+  NOVEMBER = 'November',
+  DECEMBER = 'December',
 };
