53f8acdd03f2f539c69c324f6f7b19aaa69f6f0b
Natã Elienai <nata.elienai@gmail.com>
2022-05-09 13:43:09 -0300

    feat: complete exercise 2

--- back-end/bloco-26-typescript/dia-1-introducao-ao-typescript/exercise2.ts
+++ back-end/bloco-26-typescript/dia-1-introducao-ao-typescript/exercise2.ts
@@ -1,1 +1,15 @@
 // Crie uma enum que represente os meses do ano.
+enum MonthsOfTheYear {
+  January,
+  February,
+  March,
+  April,
+  May,
+  June,
+  July,
+  August,
+  September,
+  October,
+  November,
+  December
+};
