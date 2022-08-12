93f3caf7a10b85689d77d6b3a129ecd3e36fdfc7
Natã Elienai <nata.elienai@gmail.com>
2022-08-12 12:01:49 -0300

    refactor: change Orcamento to use Imposto as strategy

--- ciencia-da-computacao/bloco-34-padroes-de-projeto/dia-2-padroes-iterator-adapter-strategy/exercise_6.py
+++ ciencia-da-computacao/bloco-34-padroes-de-projeto/dia-2-padroes-iterator-adapter-strategy/exercise_6.py
@@ -1,20 +1,30 @@
+from abc import ABC, abstractmethod
+
+
+class Imposto(ABC):
+    @abstractmethod
+    def calcular(self):
+        raise NotImplementedError
+
+
+class ImpostoIss(Imposto):
+    def calcular(self, valor):
+        return valor * 0.1
+
+
+class ImpostoIcms(Imposto):
+    def calcular(self, valor):
+        return valor * 0.06
+
+
 class Orcamento:
     def __init__(self, valor):
         self.valor = valor
 
     def calcular_imposto(self, imposto):
-        if imposto == "ISS":
-            return self.__calcular_iss()
-        elif imposto == "ICMS":
-            return self.__calcular_icms()
-
-    def __calcular_iss(self):
-        return self.valor * 0.1
-
-    def __calcular_icms(self):
-        return self.valor * 0.06
+        return imposto.calcular(self.valor)
 
 
 orcamento = Orcamento(1000)
-print(f"ISS: {orcamento.calcular_imposto('ISS')}")
-print(f"ICMS: {orcamento.calcular_imposto('ICMS')}")
+print(f"ISS: {orcamento.calcular_imposto(ImpostoIss())}")
+print(f"ICMS: {orcamento.calcular_imposto(ImpostoIcms())}")
